94a0e5c03783a913435fd98489e26e434ef4e030
nikiasek <nikiasek50@gmail.com>
2024-02-18 21:53:28 +0100

    asdasdas

--- app.js
+++ app.js
@@ -1,39 +1,89 @@
-let playerChoice, pcChoice
+let playerChoice
+let playerWin = 0
+let computerWin = 0
 
 function getComputerChoice () {
     switch(Math.floor(Math.random() * 3)) {
         case 0:
-            pcChoice = "Paper"
-            return pcChoice
+            return "paper";
         case 1:
-            pcChoice = "Rock"
-            return pcChoice
+            return "rock";
         case 2:
-            pcChoice = "Scissors"
-            return pcChoice
+            return "scissors";
     }
 }
 
 function getPlayerChoice() {
     switch(prompt("Choose between:\nRock | Paper | Scissors").toLowerCase()) {
         case "rock":
-            playerChoice = "rock"
-            return playerChoice
+            return "rock"
         case "paper":
-            playerChoice = "paper"
-            return playerChoice
+            return "paper"
         case "scissors":
-            playerChoice = "scissors"
-            return playerChoice
+            return "scissors"
         default:
-            return "špatně"
+            return getPlayerChoice()
+        }
+}
+
+function getRound(computerValue = getComputerChoice(), playerValue = getPlayerChoice()) {
+    switch(computerValue) {
+        case "paper":
+            switch(playerValue) {
+                case "paper":
+                    return "tie"
+                case "rock":
+                    return "computer"
+                case "scissors":
+                    return "player"
+            }
+        case "rock":
+            switch(playerValue) {
+                case "paper":
+                    return "player"
+                case "rock":
+                    return "tie"
+                case "scissors":
+                    return "computer"
+            }
+        case "scissors":
+            switch(playerValue) {
+                case "paper":
+                    return "computer"
+                case "rock" :
+                    return "player"
+                case "scissors":
+                    return "tie"
+            }
     }
 }
 
-function round(playerChoice, pcChoice) {
-    plCh = getPlayerChoice()
-    pcCh = getComputerChoice()
+function roundCounter(value = getRound()) {
+    if (value == "computer") {
+        computerWin++
+    }
+    else if(value == "player") {
+        playerWin++
+    }
+    else {
+        return 0
+    }
 }
 
-console.log(getComputerChoice())
-console.log("vybral sis:", getPlayerChoice())
+for(let i = 0; i < 5; i++) {
+    roundCounter()
+    if(i == 4) {
+        if(computerWin > playerWin) {
+            console.log("Computer Won by ", computerWin, "/", playerWin)
+        }
+        else if(computerWin < playerWin) {
+            console.log("Player won by ", playerWin, "/", computerWin)
+        }
+        else {
+            console.log("Tie")
+        }
+    }
+    else {
+        continue
+    }
+}
